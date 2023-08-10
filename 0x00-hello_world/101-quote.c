62727d00dd32d8a79dd254fe5f9b14ace11999c8
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-08-10 13:40:45 -0700

    fixed betty error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,7 @@
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	write(test, "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
