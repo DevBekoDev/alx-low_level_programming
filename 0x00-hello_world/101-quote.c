a4509bf83201cad572c6bb34ab903ee00e3f868a
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-08-10 13:44:24 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,10 @@
  */
 int main(void)
 {
-	write(test, "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19", 59);
+	const char msg[] = "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19";
+
+	syscall(SYS_write, 1, msg, sizeof(msg));
+
 	return (1);
 }
