be24c62a4d0ece37fef8508d62c396df3e794a31
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-08-15 10:47:30 -0700

    I wrote a C program that prints _putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-char main(void)
+/**
+ * main - Entry point
+ *
+ * Description: 'Print _putchar'
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	printf("_putchar\n");
+	return (0);
 }
