43ece6914979aceb87abd3d439d77e1409b9b1e0
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-08-18 10:53:58 -0700

    I wrote a c program that prints a square, followed by a new line.prints a square, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print-square - check the code
+ * Description: a function that prints a square, followed by a new line.
+ * @size: the size of the square
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			_putchar('#');
+			for (j = 2; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
