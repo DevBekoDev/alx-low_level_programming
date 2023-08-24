ec2d5c8320074dcb25aa4a06638e270e63c3a259
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-08-24 13:04:19 -0700

    test

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * *_cap_string - Entry
+ * Description: a function that capitalizes all words of a string.
+ * @str: pointer to a string
+ * Retunr: 0
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	int length;
+
+	length = strlen(str);
+
+	for (i = 0; i < length; i++)
+	{
+		if (i == 0)
+		{
+			str[i] = toupper(str[i]);
+		}
+		else if (str[i] == ' ' || str[i] == '.')
+		{
+			str[i + 1] = toupper(str[i + 1]);
+		}
+	}
+	return (str);
+}
