4e9bf6ceb09652aac0b50a5db5473fe56f02db24
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-08-24 13:37:11 -0700

    test

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - Entry
+ * Descrtiption: a function that encodes a string into 1337.
+ * @s: pointer to a string
+ * Return: 0
+ */
+
+char *leet(char *s)
+{
+	int i;
+
+	char a[] = {'a', 'e', 'o', 't', 'l'};
+	char leet[] = {4, 3, 0, 7, 1};
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+			{
+				*s = leet[i] + '0';
+			}
+		}
+		s++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,6 @@ void reverse_array(int *a, int n);
 int _putchar(char c);
 char *string_toupper(char *);
 char *cap_string(char *);
-
+char *leet(char *);
 
 #endif
