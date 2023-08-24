969132dd910dc77d22e72adb9d382a9259b4e437
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-08-24 13:47:25 -0700

    I wrote a c program that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,6 +10,7 @@
 char *leet(char *s)
 {
 	int i;
+	char *r =s;
 
 	char a[] = {'a', 'e', 'o', 't', 'l'};
 	char leet[] = {4, 3, 0, 7, 1};
@@ -25,5 +26,6 @@ char *leet(char *s)
 		}
 		s++;
 	}
-	return (s);
+
+	return r;
 }
