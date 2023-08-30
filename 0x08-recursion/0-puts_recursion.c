144209502e1e4cc204f6f0fef23e1af093218873
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-08-30 15:43:44 -0700

    I wrote a c program that  prints a string, followed by a new line.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts_recursion - Entry
+ * Description: a function that prints a string, followed by a new line.
+ * @s: pointer to a string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
