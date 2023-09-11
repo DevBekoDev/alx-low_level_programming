aef5a9aed9617944ac1b1dd90ffdc22ef551f57a
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-09-11 15:37:47 -0700

    fixed the logic of my function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,20 +9,10 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
+	if (d != NULL)
 	{
-		d->name = "(nil)";
-	}
-	else if (isnanf(d->age))
-	{
-		d->age = 0;
-	}
-	else if (d->owner == NULL)
-	{
-		d->owner = "(nil)";
-	}
-	else if (d != NULL)
-	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
