8a5cff277f9629120ab0e2beb41760666a089470
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-09-11 15:04:44 -0700

    fixed the pssibablity of d being null

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdio.h>
 /**
  * init_dog -  initialize a variable of type struct dog
  * @d: name of the varaible
@@ -11,7 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
