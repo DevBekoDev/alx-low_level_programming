760d6f473f4bb7a20114aadbab696571bfd692ba
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-09-11 16:31:25 -0700

    test

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Free the memory of dog_t type of object
+ * @d: dog_t type of object
+ * Return: Void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
