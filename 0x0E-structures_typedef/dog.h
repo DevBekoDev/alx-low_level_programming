bd4cd3065b143dca10241f4bfaf991742e023f14
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-09-11 14:23:59 -0700

    I defined a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dog info
+ * @name: name of the dog
+ * @owner: name of the owner
+ * @age: age of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
