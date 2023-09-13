0634c197b1ce03f543fe2dee53bf3dd04d51aefa
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-09-13 13:36:24 -0700

    I followed some instructions i was not allowed to do

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,15 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 4)
+	if (argc != 4)
 	{
-		printf("Enter two numbers and the operation u want\n");
-		return (1);
+		printf("Error\n");
+		exit(98);
 	}
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
-		return (1);
+		exit(99);
 	}
 
 	printf("%i\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
