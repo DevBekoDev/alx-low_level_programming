fa03ccf0165ee468e76c30e43cf28e5abc9ca55a
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-09-13 13:41:05 -0700

    Considerd the division by zero

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,6 +22,11 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	printf("%i\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
 	return (1);
 }
